5bfe1275016225d045c526c0cd22f0b7c8d4daf3
RamaRamdani03 <ramaramdani449@gamil.com>
2023-05-24 11:09:45 +0700

    membuat class orang

--- ParadigmaOOP/ParadigmaOOP.cpp
+++ ParadigmaOOP/ParadigmaOOP.cpp
@@ -2,27 +2,4 @@
 #include <string>
 using namespace std;
 
-class orang {
-public:
-	string nama;
-
-	orang(string pNama) :
-		nama(pNama) {
-		cout << "orang dibuat\n" << endl;
-	}
-	~orang() {
-		cout << "orang dihapus\n" << endl;
-	}
-
-	int jumlah(int a, int b) {
-		return a + b;
-	}
-
-};
-
-class pelajar : public orang {
-public: 
-	string sekolah;
-
-	pelajar(string pNama, string pSekolah);
-};
+class orang 
