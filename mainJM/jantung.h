b7663390eaa7a3c3305ba5585a8e29709a15ddd3
RamaRamdani03 <ramaramdani449@gamil.com>
2023-05-24 21:32:15 +0700

    class jantung dengan hak access public dan akan menghasilkan output jantung dihidupkan atau dimatikan

--- mainJM/jantung.h
+++ mainJM/jantung.h
@@ -3,4 +3,13 @@
 
 using namespace std;
 
-class jantung 
+class jantung {
+public:
+	jantung() {
+		cout << "Jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "Jantung dimatikan\n";
+	}
+};
+#endif
